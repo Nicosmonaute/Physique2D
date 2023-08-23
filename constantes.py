c3d3096cf77fef4a43f9393eaff5a120587bcc18
Nicolas Moreau de Lizoreux <nicoco1203moro@gmail.com>
2023-08-23 18:36:03 +0200

    ajout de quelques commentaires

--- constantes.py
+++ constantes.py
@@ -1,5 +1,5 @@
 WINDOW_SIZE=(700, 700) # dimension de la fenêtre en pixels
 G=9.81 # constante gravitationnelle
-METTER=100 # 1 m = 100 px
-RHO=3000 # kg / m^3
-COEFF_FROTTEMENT=0.5 # unité inconnue, mais ça marche
+METTER=100 # nombre de pixels pour un mètre
+RHO=3000 # masse volumique kg / m^3
+COEFF_FROTTEMENT=0.5 # valeur arbitraire pour la force de frottement
